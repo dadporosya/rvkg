aecb10cca9577eb9544bb8da79664b11cd1d0b2d
ricardsjansons <ricardsjansons17@gmail.com>
2025-10-06 13:11:24 +0300

    .

--- static/js/roulette.js
+++ static/js/roulette.js
@@ -1,7 +1,22 @@
+let angle = 0;
+let speed = 0;
+let acc = -1;
+
 $(function () {
   upd_score();
   $("button").on("click", spin)
+  setInterval(upd_roulette, 10);
 });
+
+function upd_roulette() {
+  if (speed > 0 && speed + acc <= 0) {
+    upd_score();
+  }
+  speed = Math.max(0, speed + acc);
+  angle += speed;
+  angle %= 360;
+  $("#roulette").css({ transform: `rotate(${angle}deg)` });
+}
 
 function upd_score() {
   $.get(`/get-score`, function (data, status) {
@@ -10,13 +25,7 @@ function upd_score() {
 }
 
 function spin() {
+  speed = 50
   bet = $("input").val();
-  $.get(`/roulette/spin/${bet}`, function (data, status) {
-    if (data == "1") {
-      $("#result").html("win");
-    } else {
-      $("#result").html("loss");
-    }
-    upd_score();
-  });
+  $.get(`/roulette/spin/${bet}`);
 }
